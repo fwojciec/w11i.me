648de599a0d5aa2ef40b2be40a516ba494b41355
Filip Wojciechowski <fwojciec@gmail.com>
2019-07-12 13:44:48 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,132 @@
+require('dotenv').config()
+const postCssPresetEnv = require(`postcss-preset-env`)
+const postCSSNested = require('postcss-nested')
+const postCSSUrl = require('postcss-url')
+const postCSSImports = require('postcss-import')
+const cssnano = require('cssnano')
+const postCSSMixins = require('postcss-mixins')
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://w11i.me',
+    title: `w11i's blog`,
+    description: `A personal blog featuring writing on my two hobbies: coding and books.`,
+    copyrights: `Text copyright @ ${new Date().getFullYear()}, Filip Wojciechowski.`,
+    author: 'Filip Wojciechowski',
+    logo: {
+      src: '',
+      alt: ''
+    },
+    logoText: 'w11i',
+    defaultTheme: 'dark',
+    postsPerPage: 5,
+    showMenuItems: 1,
+    menuMoreText: 'Show more',
+    mainMenu: [
+      {
+        title: 'About',
+        path: '/about'
+      }
+    ]
+  },
+  plugins: [
+    'babel-preset-gatsby',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName: process.env.S3_BUCKET_NAME,
+        protocol: 'https',
+        hostname: 'w11i.me'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          postCSSUrl(),
+          postCSSImports(),
+          postCSSMixins(),
+          postCSSNested(),
+          postCssPresetEnv({
+            importFrom: 'src/styles/variables.css',
+            stage: 1,
+            preserve: false
+          }),
+          cssnano({
+            preset: 'default'
+          })
+        ]
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-embed-video',
+            options: {
+              related: false,
+              noIframeBorder: true
+            }
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+              quality: 100,
+              showCaptions: true,
+              linkImagesToOriginal: false
+            }
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `w11i's blog`,
+        short_name: 'w11i',
+        start_url: '/',
+        background_color: '#292a2d',
+        theme_color: '#292a2d',
+        display: 'minimal-ui',
+        icon: 'src/images/w11i-icon.png'
+      }
+    }
+  ]
+}
---
+++ src/pages/about.md
@@ -0,0 +1,10 @@
+---
+title: 'About'
+date: '2018-07-19'
+path: '/about'
+---
+
+Hi, my name is **Filip**. I'm a literary agent, a husband and a father. I like to code in what remains of my free time. On this blog I write about coding and books. I also sometimes post things on [Twitter](https://twitter.com/filipcodes).
+
+This blog is created using [Gatsby](https://www.gatsbyjs.org/).
+Starter theme by [panr](https://radoslawkoziel.pl).
